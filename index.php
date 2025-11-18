5937d29b58d7f9cba401865c6eac1db44a159bae
XymphonyS2 <140775363+XymphonyS2@users.noreply.github.com>
2025-11-18 21:46:29 +0700

    dasar

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Manajemen Kontak</title>
+</head>
+<body>
+    <h1>Sistem Manajemen Kontak</h1>
+</body>
+</html>
